2c8622e1eab64c61838a40cf8a373252e8cd0dc7
GStoppa <gustavovieirastoppa50@gmail.com>
2026-09-13 20:47:02 -0300

    Blueprint no init

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,11 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__, template_folder="view", static_folder='static')
-
     app.secret_key = 'sla_qualquer_coisa'
+
+    #importar o Blueprint do controller
+    from app.controllers.controller import tarefasbp
+    #registra o blueprint do controller na aplicação
+    app.register_blueprint(tarefasbp)
 
     return app
--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template, redirect
 
-
 #Inicializao do Blueprint
 tarefasbp = Blueprint('tarefas', __name__)
 
