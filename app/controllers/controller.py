780e74e83db0078f3fbe074c778c99af288f9ec7
GStoppa <gustavovieirastoppa50@gmail.com>
2026-09-13 19:42:17 -0300

    finalizao do tarefas.py e comeco do controller

---
+++ app/controllers/controller.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template, redirect
+
+
+#Inicializao do Blueprint
+tarefasbp = Blueprint('tarefas', __name__)
+
+@tarefasbp.route('/')
+def index():
+    return render_template("index.html")
--- app/models/tarefa.py
+++ app/models/tarefa.py
@@ -1,5 +1,26 @@
-class Tarefa():
-    def __init__(self, id_tarefa, descricao, estado_bool = False):
+#Tarefa (cls)
+class Tarefa:
+    def __init__(self, id_tarefa, descricao, estado):
         self.id_tarefa = id_tarefa
         self.descricao = descricao
-        self.estado_bool = estado_bool
+        self.estado = estado
+
+    def alternar_status(self):
+        self.estado = not self.estado
+
+    def para_dict(self):
+        return {
+            "id_tarefa": self.id_tarefa,
+            "descricao": self.descricao,
+            "estado": self.estado,
+        }
+
+    #vai receber toda a classe (e não só um self especifico)
+    @classmethod
+    def from_dict(cls, dados):
+        #cls é mesma coisa que Tarefa(...)
+        return cls(
+            id_tarefa=dados["id_tarefa"],
+            descricao=dados["descricao"],
+            estado=dados.get("estado", False)
+        )
