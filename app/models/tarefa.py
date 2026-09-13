0c6c7c1a4ccfc76d2ed081a4a4601411a38ec006
GStoppa <gustavovieirastoppa50@gmail.com>
2026-09-13 18:57:23 -0300

    self tarefas

---
+++ app/models/tarefa.py
@@ -0,0 +1,5 @@
+class Tarefa():
+    def __init__(self, id_tarefa, descricao, estado_bool = False):
+        self.id_tarefa = id_tarefa
+        self.descricao = descricao
+        self.estado_bool = estado_bool
