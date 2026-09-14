49af4185db773432ed63ca8141c01eca6dc867e8
GStoppa <gustavovieirastoppa50@gmail.com>
2026-09-13 21:00:26 -0300

    Comecando Jinja2!

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__, template_folder="view", static_folder='static')
+    app = Flask(__name__, template_folder="views", static_folder='static')
     app.secret_key = 'sla_qualquer_coisa'
 
     #importar o Blueprint do controller
--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, render_template, redirect
+from app.models.tarefa import Tarefa
 
 #Inicializao do Blueprint
 tarefasbp = Blueprint('tarefas', __name__)
 
 @tarefasbp.route('/')
 def index():
-    return render_template("index.html")
+    lista_tarefas = [
+        Tarefa(1, "Estudar Flask"),
+        Tarefa(2, "Praticar MVC", estado=True),
+    ]
+    return render_template("index.html", tarefas=lista_tarefas)
